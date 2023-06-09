170a5e5fa22c3ee705e87f7448525b1836488478
DavidHervas12 <121380511+DavidHervas12@users.noreply.github.com>
2023-06-09 11:02:07 +0200

    Simulate database with json file

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-$contacts = [
-  ["name" => "Pepe", "phone_number" => "232342"],
-  ["name" => "Antonio", "phone_number" => "432343"],
-  ["name" => "Nate", "phone_number" => "092399"],
-  ["name" => "Rodrigo", "phone_number" => "457543"],
-  ["name" => "Jaime", "phone_number" => "978654"]
-]
+if (file_exists("contacts.json")) {
+  $contacts = json_decode(file_get_contents("contacts.json"), true);
+} else {
+  $contacts = [];
+}
   ?>
 
 <!DOCTYPE html>
@@ -47,7 +45,7 @@ $contacts = [
             <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./add.html">Add Contact</a>
+            <a class="nav-link" href="./add.php">Add Contact</a>
           </li>
         </ul>
       </div>
@@ -58,6 +56,14 @@ $contacts = [
     <div class="container pt-4 p-3">
       <div class="row">
 
+      <?php if (count($contacts) == 0): ?>
+        <div class="col-md-4 mx-auto">
+          <div class="card card-body text-center">
+            <p>No contacts saved yet</p>
+            <a href="./add.php">Add One</a>
+          </div>
+        </div>
+        <?php endif ?>
       <?php foreach ($contacts as $contact):  ?>
         <div class="col-md-4 mb-3">
           <div class="card text-center">
