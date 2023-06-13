6a56cc4bdc5d95e003ddadff6f83ab248a23eb2b
DavidHervas12 <121380511+DavidHervas12@users.noreply.github.com>
2023-06-13 21:12:21 +0200

    Create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,17 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+DROP TABLE users;
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+INSERT INTO users (name, email, password) VALUES ('David', 'dhervas456@gmail.com', '1234');
+
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
@@ -9,3 +20,5 @@ CREATE TABLE contacts (
 );
 
 INSERT INTO contacts (name, phone_number) VALUES ("Juanjo", "9328572389");
+
+
